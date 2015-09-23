4f41efdba6cbc9938505dc1384ff6ec54c6ad5f4
Minty Zhang <minty.mz@gmail.com>
2015-09-23 11:41:09 -0400

    Starter code

---
+++ Starter/compile.sh
@@ -0,0 +1,2 @@
+g++ -O4 -g -fopenmp Boids.cpp -L./ -I./ -l3ds -lGL -lglut -lGLU -lglui -lm -o Boids
+
---
+++ Starter/run.sh
@@ -0,0 +1,6 @@
+export LD_LIBRARY_PATH=./
+echo $1
+echo $2
+echo $3
+echo $4	 # FILE NAME SHOULD CONTAIN NO SPACES
+./Boids $1 $2 $3 $4
