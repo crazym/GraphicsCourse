0330909b671fa9eb50f17e0b984bf844a9298c9f
Maoting Zhang <maoting.zhang@mail.utoronto.ca>
2015-12-01 17:27:40 -0500

    quick fix on dpf

--- a4/starter/RayTracer.c
+++ a4/starter/RayTracer.c
@@ -24,7 +24,7 @@
 
 #define AASamples 5
 #define dof_samples        3
-// larger aperture_size, wider range of depth of fields
+// smaller aperture_size, wider range of depth of fields
 #define aperture_size   3
 #define focal_length     2.5
 
